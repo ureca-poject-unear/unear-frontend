57d3d7f10200c3debe3be569d49cc2c6f918d579
khwww <khw4420@knu.ac.kr>
2025-07-16 04:33:07 +0900

    feat: 메인페이지 생성 및 스타일 확인을 위한 내용 세팅

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,11 +2,40 @@ import React from 'react';
 
 const MainPage = () => {
   return (
-    <>
+    <div className="">
       <div className="max-w-4xl mx-auto text-center">
-        <h1 className="text-4xl font-bold mb-4 text-blue-600">UNEAR 프론트엔드 프로젝트</h1>
+        <h1 className="text-lg font-bold mb-2 text-primary">U:NEAR 프론트엔드 프로젝트</h1>
+
+        <div className="max-w-4xl mx-auto text-center space-y-4">
+          <h2 className="text-m font-thin text-primary">Thin - LGEIHeadline‑Thin</h2>
+
+          <h2 className="text-m font-light text-primary">Light - LGEIHeadline‑Light</h2>
+
+          <h2 className="text-m font-regular font-normal text-primary">
+            Regular - LGEIHeadline‑Regular
+          </h2>
+          <h2 className="text-m font-semibold text-primary">Semibold - LGEIHeadline‑Semibold</h2>
+
+          <h2 className="text-m font-bold text-primary">Bold - LGEIHeadline‑Bold</h2>
+        </div>
+        <p className="mt-8 text-sm font-bold text-black mb-6">스타일 테스트</p>
+        <div
+          className="
+            w-24 h-24 
+            bg-storeicon        
+            rounded-full 
+            mx-auto 
+            mb-6
+          "
+        />
+
+        <div className="w-32 h-12 bg-store rounded-[12px] mx-auto mb-6" />
+
+        <button className="bg-primary text-white px-6 py-2 rounded-[12px] text-lm">
+          테스트 버튼
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
