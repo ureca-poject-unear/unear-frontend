710fe26fe1d6a44b70d80bff5faa5d9d1642d26a
khwww <khw4420@knu.ac.kr>
2025-07-16 04:32:22 +0900

    feat: defalut layout 세팅

--- src/default.tsx
+++ src/default.tsx
@@ -3,9 +3,16 @@ import { Outlet } from 'react-router-dom';
 
 const Default = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    //min-h-[852px]
+    <div className="w-full max-w-[393px] min-h-screen bg-background mx-auto flex flex-col px-5">
+      {/* <Header /> */}
+
+      <main>
+        <Outlet />
+      </main>
+
+      {/* <Nav /> */}
+    </div>
   );
 };
 
