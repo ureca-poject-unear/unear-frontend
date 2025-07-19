aef77ab515e6da7d05182100c59440538a954dff
khwww <khw4420@knu.ac.kr>
2025-07-20 04:56:25 +0900

    [WL7-209]chore: 지도페이지 레이아웃 수정

--- src/default.tsx
+++ src/default.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import BottomNavigator from '@/components/common/BottomNavigator';
+import { useLocation } from 'react-router-dom';
 
 const Default = () => {
+  const { pathname } = useLocation();
+  const isMapPage = pathname === '/map';
   return (
-    <div className="w-full max-w-[393px] min-h-screen bg-background mx-auto flex flex-col relative px-5">
-      <main className="pt-[40px] pb-[65px]">
+    <div
+      className={`w-full max-w-[393px] min-h-screen bg-background mx-auto flex flex-col relative ${
+        isMapPage ? '' : 'px-5'
+      }`}
+    >
+      <main className={isMapPage ? '' : 'pt-[40px] pb-[65px]'}>
         <Outlet />
       </main>
 
