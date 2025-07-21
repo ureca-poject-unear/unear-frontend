dfa79185798aec87439145e873715f65980463d0
KimJunSeo <rlawnstj901+2@gmail.com>
2025-07-21 16:00:16 +0900

    chore: default 좌우 여백 수정 및 메인페이지 여백 수정

--- src/default.tsx
+++ src/default.tsx
@@ -5,12 +5,9 @@ import { useLocation } from 'react-router-dom';
 const Default = () => {
   const { pathname } = useLocation();
   const isMapPage = pathname === '/map';
+
   return (
-    <div
-      className={`w-full max-w-[393px] min-h-screen bg-background mx-auto flex flex-col relative ${
-        isMapPage ? '' : 'px-5'
-      }`}
-    >
+    <div className="w-full max-w-[393px] min-h-screen bg-background mx-auto flex flex-col relative">
       <main className={isMapPage ? '' : 'pt-[40px] pb-[77px]'}>
         <Outlet />
       </main>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,10 @@
-import Grade from '../components/common/Grade';
-
-import _404State from '@/components/common/404State';
-
 import BarcodeIcon from '@/assets/common/barcode.svg?react';
 import InformationIcon from '@/assets/common/information.svg?react';
 import MainNubiImage from '@/assets/MainPage/mainnubi.svg?react';
 import EventBannerImage from '@/assets/MainPage/eventbanner.png';
 import StoryBackgroundImage from '@/assets/MainPage/storyBackground.png';
 import StoryNubiImage from '@/assets/MainPage/storynubi.svg?react';
-
+import Grade from '@/components/common/Grade';
 import BottomSheetBarcode from '@/components/common/BottomSheetBarcode';
 import MembershipBrandBanner from '@/components/MainPage/MembershipBrandBanner';
 import MembershipBenefitModal from '@/components/MainPage/MembershipBenefitModal';
@@ -57,7 +53,7 @@ const MainPage = () => {
       </header>
 
       {/* 메인 컨텐츠 영역 */}
-      <main>
+      <main className="px-5">
         {/* 사용자 카드 */}
         <div
           className="w-full bg-white rounded-[20px] mt-3 pl-6 pt-3 pb-3 flex items-center justify-between"
