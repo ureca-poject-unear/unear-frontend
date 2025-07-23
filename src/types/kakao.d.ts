362dc47c182601adc8ea19d75b3fedddb7251969
khwww <khw4420@knu.ac.kr>
2025-07-23 15:47:59 +0900

    [WL7-209]feat: 카카오맵 타입 추가

--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -1,30 +1,47 @@
 // src/types/kakao.d.ts
+export interface KakaoLatLng {
+  getLat(): number;
+  getLng(): number;
+}
+
+export interface KakaoMap {
+  setCenter(latlng: KakaoLatLng): void;
+  setLevel(level: number): void;
+}
+
+export interface KakaoMapOptions {
+  center: KakaoLatLng;
+  level: number;
+}
+
+export interface KakaoMarker {
+  setPosition: (latlng: kakao.maps.LatLng) => void;
+  setMap: (map: kakao.maps.Map | null) => void;
+  getPosition: () => kakao.maps.LatLng;
+}
+
+export interface KakaoCustomOverlay {
+  setMap(map: KakaoMap | null): void;
+  setPosition(position: KakaoLatLng): void;
+}
 
 declare global {
-  interface KakaoLatLng {
-    getLat(): number;
-    getLng(): number;
-  }
-
-  interface KakaoMap {
-    setCenter(latlng: KakaoLatLng): void;
-    setLevel(level: number): void;
-  }
-
-  interface KakaoMapOptions {
-    center: KakaoLatLng;
-    level: number;
-  }
-
   interface Window {
     kakao: {
       maps: {
         LatLng: new (lat: number, lng: number) => KakaoLatLng;
         Map: new (container: HTMLElement, options: KakaoMapOptions) => KakaoMap;
+        Marker: new (options: { map: KakaoMap; position: KakaoLatLng }) => KakaoMarker;
         load(callback: () => void): void;
         MapTypeId: unknown;
         ZoomControl: new () => unknown;
         services?: unknown;
+        CustomOverlay: new (options: {
+          position: KakaoLatLng;
+          content: Node;
+          yAnchor?: number;
+          zIndex?: number;
+        }) => KakaoCustomOverlay;
       };
     };
   }
