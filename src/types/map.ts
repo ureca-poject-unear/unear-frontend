9b5811de165331e4c6dba1a39a7be0116b5ac55c
khwww <khw4420@knu.ac.kr>
2025-07-25 14:41:49 +0900

    [WL7-209]feat: 지도 타입 추가

---
+++ src/types/map.ts
@@ -0,0 +1,26 @@
+export type CategoryType =
+  | 'FOOD'
+  | 'ACTIVITY'
+  | 'EDUCATION'
+  | 'CULTURE'
+  | 'BAKERY'
+  | 'LIFE'
+  | 'SHOPPING'
+  | 'CAFE'
+  | 'BEAUTY'
+  | 'POPUP';
+
+export type StoreClassType = 'LOCAL' | 'FRANCHISE' | 'BASIC';
+
+export type EventType = 'NONE' | 'GENERAL' | 'REQUIRE';
+
+export interface Place {
+  id: number;
+  latitude: number;
+  longitude: number;
+  categoryCode: CategoryType;
+  markerCode: StoreClassType;
+  eventCode: EventType;
+  benefitCategory: string;
+  favorite: boolean;
+}
