977246ffe1c698b96e3e71dd0e38364369e56c24
khwww <khw4420@knu.ac.kr>
2025-07-27 04:51:43 +0900

    [WL7-209]feat: 가까운 장소 및 쿠폰 템플릿 타입 설정

--- src/apis/getNearbyStores.ts
+++ src/apis/getNearbyStores.ts
@@ -4,5 +4,6 @@ export const getNearbyStores = async (latitude: number, longitude: number) => {
   const res = await axiosInstance.get('/places/nearby-with-coupons', {
     params: { latitude, longitude },
   });
+  console.log('🎯 주변매장과 쿠폰 응답:', res.data);
   return res.data.data;
 };
