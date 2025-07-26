b3e92da18d17d8a67ea0a2bda5bc3181a137a042
khwww <khw4420@knu.ac.kr>
2025-07-27 06:39:35 +0900

    [WL7-209]chore: chore: axios 배열 쿼리 파라미터 처리를 위한 qs 설치 및 인스턴스에 적용

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
+import qs from 'qs';
 
 const axiosInstance = axios.create({
   baseURL: 'https://dev.unear.site/api/app',
   headers: {
     'Content-Type': 'application/json',
   },
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
 });
 
-//토근 전역 상태관리 전까지 테스트용도
+// 토큰 전역 상태관리 전까지 테스트용도
 axiosInstance.interceptors.request.use((config) => {
-  const token = 'TOKEN값';
+  const token = 'Token값';
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
