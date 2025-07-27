c952c222f42c100b1fa584daab869ca6bfaa408e
khwww <khw4420@knu.ac.kr>
2025-07-27 18:57:38 +0900

    [WL7-221]feat: 쿠폰 다운로드 api 인스턴스 연동

--- src/apis/toggleFavorite.ts
+++
@@ -1,6 +0,0 @@
-import axiosInstance from './axiosInstance';
-
-export const toggleFavorite = async (placeId: number) => {
-  const response = await axiosInstance.post(`/places/${placeId}/favorite`);
-  return response.data.data;
-};
